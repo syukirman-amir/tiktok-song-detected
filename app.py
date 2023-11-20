612ff02eab30907e1b9a6a033ab495008e785d08
Syukirman Amir <128995740+syukirman-amir@users.noreply.github.com>
2023-11-20 09:10:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,28 @@ from shazamio import Shazam
 from urllib.parse import urlparse, urlencode
 from bs4 import BeautifulSoup
 
+# Function to extract the path from a redirected TikTok URL
+def get_redirected_path(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return urlparse(response.url).path
+    return None
+
 # Function to extract the path from a full TikTok URL
 def get_tiktok_path(url):
     parsed_url = urlparse(url)
     path = parsed_url.path
 
-    if path.startswith('/'):
-        path = path[1:]  # Removing the '/' character at the beginning of the path
+    if parsed_url.netloc == "www.tiktok.com" and path.startswith('/'):
+        return path[1:]
+    elif parsed_url.netloc == "vt.tiktok.com":
+        redirected_path = get_redirected_path(url)
+        if redirected_path:
+            return redirected_path[1:] if redirected_path.startswith('/') else redirected_path
 
-    return path
+    return None
+
+
 
 # Function to get the last part of the TikTok URL
 def get_last_part(base_url, path):
@@ -71,26 +84,33 @@ def get_play_url(base_url, path, additional_parameters=None):
 
 # Function to detect the song from the audio URL content
 async def detect_song_from_url(play_url):
-    response = requests.get(play_url)
-    result = await recognize_song(response.content)
-    
-    if result:
-        data = result['track']['sections']
-        yturl, titlesong = None, None
-        for item in data:
-            youtube_url = item.get('youtubeurl')
-            if youtube_url:
-                youtube_response = requests.get(youtube_url)
-                json_data = youtube_response.json()
-                list_item = json_data.get('actions', [])
-                for youitem in list_item:
-                    yturl = youitem.get('uri')
-                    judul = youitem.get('share', [])
-                    if judul:
-                        titlesong = judul.get('text', [])
-                    return yturl, titlesong
+    try:
+        response = requests.get(play_url)
+        result = await recognize_song(response.content)
+        
+        if result and 'track' in result:
+            data = result['track']['sections']
+            yturl, titlesong = None, None
+            for item in data:
+                youtube_url = item.get('youtubeurl')
+                if youtube_url:
+                    youtube_response = requests.get(youtube_url)
+                    json_data = youtube_response.json()
+                    list_item = json_data.get('actions', [])
+                    for youitem in list_item:
+                        yturl = youitem.get('uri')
+                        judul = youitem.get('share', [])
+                        if judul:
+                            titlesong = judul.get('text', [])
+                        return yturl, titlesong
 
-    return None, None
+        # Jika 'track' tidak ada dalam respon atau tidak ada data lagu yang ditemukan
+        raise KeyError("Track information not found or song not detected.")
+
+    except Exception as e:
+        print(f"Error detecting song: {str(e)}")
+        return None, None
+
 
 # Function to recognize the song from audio content using Shazam
 async def recognize_song(file):
@@ -102,7 +122,7 @@ def main():
     st.title('TikTok Detected Music')
     st.caption("Made with 💜  Syukirman Amir")
     st.write("Enter the complete TikTok URL:")
-    full_url = st.text_input("Example: https://www.tiktok.com/@username/video/1234567890")
+    full_url = st.text_input("Example: https://www.tiktok.com/@username/video/1234567890 OR https://vt.tiktok.com/ZSNacef3m/")
     
     if st.button("Detected Music"):
         base_url = 'https://www.tiktok.com/'
@@ -111,9 +131,6 @@ def main():
         if path:
             play_url = get_play_url(base_url, path)
             if play_url:
-               # st.success(f"Desired URL: {play_url}")
-                #st.audio(play_url)  # Displaying the audio using Streamlit's audio widget
-
                 loop = asyncio.new_event_loop()
                 asyncio.set_event_loop(loop)
                 yturl, titlesong = loop.run_until_complete(detect_song_from_url(play_url))
